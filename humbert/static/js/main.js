ceb10bd93eccf57695439a2f45075e0d883b9e6d
Peter Zakin <peter.zakin@gmail.com>
2013-04-04 00:36:57 -0400

    first draft of highlighting

--- humbert/static/js/main.js
+++ humbert/static/js/main.js
@@ -1,7 +1,3 @@
-
-
-
-
 $(document).ready(function(){
 
     csrf_token = $('#csrf_token').val()
@@ -76,44 +72,111 @@ $(document).ready(function(){
 
     }
 
-    highlight = function(){
+    $('aside').mouseup(function(e){
+        e.stopPropagation();
+    });
 
+/*    $(document).mouseup(function(){
         
-    }
-    
-
-    $('section span').mousedown().$(document).mouseup(function(){
-
-
-    var t = '';
+   var t = '';
     if(window.getSelection){
-        t = window.getSelection();
+        t = window.getSelection().toString();
     }else if(document.getSelection){
-        t = document.getSelection();
+        t = document.getSelection().text;
     }else if(document.selection){
         t = document.selection.createRange().text;
     }
+//        console.log(t); 
         alert(t);
+  
+  
+        
 
+    }); */
+
+
+
+
+
+    var timeout;
+    var last_span = 0;
+    var HIGHLIGHT_COLOR = 'red';
+
+
+    //sets a new last_span and percolates the highlight accordingly
+    expand_highlight = function(new_span){
+        new_last_span = parseInt(new_span.attr('id'));
+        console.log('new last_span' + new_span.attr('id'));
+        
+
+        //needs to highlight all of the spans between the old last span and the newest last span
+        for (var id=parseInt(last_span); id <= new_last_span; id++){
+            console.log("its colored!");
+            $('#' + id).addClass('highlighted');
+        }
+        
+        console.log('new last span is' + new_last_span);
+        last_span = new_last_span;
+    };
+
+    minimize_highlight = function(new_span){
+        new_last_span = parseInt(new_span.attr('id'));
+        
+        //iterate through
+        for (var id=new_last_span; id <= parseInt(last_span); id++){
+            console.log("its colored!");
+            $('#' + id).removeClass('highlighted');
+        }
+        last_span = new_last_span;
+    }
+
+
+    //makes sure that any other annotations are cleared away before starting a new one
+    clean_up = function(){
+        $('span.highlight').removeClass('highlight');
+    }
+
+    $('#annotation').mousedown(function(e){
+        e.preventDefault();
+        clean_up();
+        
+        timeout = setInterval(function(){ 
+            $('span').hover(function(){
+                $(this).addClass('highlighted');
+                if (parseInt($(this).attr('id')) > last_span){
+                    expand_highlight($(this));
+                }
+
+                if(parseInt($(this).attr('id')) < last_span){
+                    minimize_highlight($(this));
+                }
+            });
+
+        }, 50)
+
+    }).mousemove(function(e){
+        e.preventDefault();
     });
 
+    $(document).mouseup(function(e){
+        e.preventDefault();
+        console.log('mouseup');
+        clearInterval(timeout);
+        $('span').unbind();
+        return false;
+        
     });
+
+
+
 /*    $('#create_annotation').click(function(){
         create_annotation();
     });*/
 
+   //i think that getting selection is probably a bad idea after all. You might as well just use mousedown and keep a tally onthe spans.
+
 
 });
-
-
-
- 
-
-
-
-
-
-
 
 
 
